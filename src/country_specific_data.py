6e0c87c3ac171f5d0892c7a912ccc516dad758eb
konspras <konstantinos.prasopoulos@epfl.ch>
2025-05-29 11:02:18 +0200

    Add datasets for fuel stories

--- src/country_specific_data.py
+++ src/country_specific_data.py
@@ -52,7 +52,93 @@ def ukraine(data_df, epc22_df, pc_df, cc_df, gdp_df, cpi_df):
     ratio.reset_index(inplace=True)
     save_dataframe_to_csv(ratio, f"{OUTPUT_DIR_COUNTRY}/UKR/cereal_exports.csv")
 
+def usa(data_df, epc22_df, pc_df, cc_df, gdp_df, cpi_df):
+    # Want a CSV with year,trade_balance_usd
+    df = data_df.copy()
+    df = df[df["product_chapter"] == "27"]  # fuels
+    exports = df[df["exporter"] == 842]
+    imports = df[df["importer"] == 842]  # USA
+    total_exports = exports.groupby(["year"]).agg({"value_trln_USD": "sum", "quantity_mln_metric_tons": "sum"})
+    total_imports = imports.groupby(["year"]).agg({"value_trln_USD": "sum", "quantity_mln_metric_tons": "sum"})
+    total_exports.reset_index(inplace=True)
+    total_imports.reset_index(inplace=True)
+    to_csv = {"year": total_exports["year"],
+              "imports_trln_USD": total_imports["value_trln_USD"],
+              "imports_mln_metric_tons": total_imports["quantity_mln_metric_tons"],
+              "exports_trln_USD": total_exports["value_trln_USD"],
+              "exports_mln_metric_tons": total_exports["quantity_mln_metric_tons"],
+              "balance_trln_USD": total_exports["value_trln_USD"] - total_imports["value_trln_USD"],
+              "balance_mln_metric_tons": total_exports["quantity_mln_metric_tons"] - total_imports["quantity_mln_metric_tons"],
+              }
+    save_dataframe_to_csv(pd.DataFrame(to_csv), f"{OUTPUT_DIR_COUNTRY}/USA/fuel_trade.csv")
+
+def iran(data_df, epc22_df, pc_df, cc_df, gdp_df, cpi_df):
+    # Want a CSV with year,trade_balance_usd
+    df = data_df.copy()
+    df = df[df["product_chapter"] == "27"]  # fuels
+    exports = df[df["exporter"] == 364]
+    imports = df[df["importer"] == 364]  # Iran
+    total_exports = exports.groupby(["year"]).agg({"value_trln_USD": "sum", "quantity_mln_metric_tons": "sum"})
+    total_imports = imports.groupby(["year"]).agg({"value_trln_USD": "sum", "quantity_mln_metric_tons": "sum"})
+    total_exports.reset_index(inplace=True)
+    total_imports.reset_index(inplace=True)
+    to_csv = {"year": total_exports["year"],
+              "imports_trln_USD": total_imports["value_trln_USD"],
+              "imports_mln_metric_tons": total_imports["quantity_mln_metric_tons"],
+              "exports_trln_USD": total_exports["value_trln_USD"],
+              "exports_mln_metric_tons": total_exports["quantity_mln_metric_tons"],
+              "balance_trln_USD": total_exports["value_trln_USD"] - total_imports["value_trln_USD"],
+              "balance_mln_metric_tons": total_exports["quantity_mln_metric_tons"] - total_imports["quantity_mln_metric_tons"],
+              }
+    save_dataframe_to_csv(pd.DataFrame(to_csv), f"{OUTPUT_DIR_COUNTRY}/IRN/fuel_trade.csv")
+
+def china(data_df, epc22_df, pc_df, cc_df, gdp_df, cpi_df):
+    # Want a CSV with year,trade_balance_usd,%of_total
+    # And another with top import sources for 2023, ie country,import_amnount_usd,import_amount_weight
+
+    # First timeseries CSV
+    df = data_df.copy()
+    df = df[df["product_chapter"] == "27"]  # fuels
+    exports = df[df["exporter"] == 156]
+    imports = df[df["importer"] == 156]  # China
+    imports_sans_chn = df[df["importer"] != 156].copy()
+    total_exports = exports.groupby(["year"]).agg({"value_trln_USD": "sum", "quantity_mln_metric_tons": "sum"})
+    total_imports = imports.groupby(["year"]).agg({"value_trln_USD": "sum", "quantity_mln_metric_tons": "sum"})
+    total_imp_sans = imports_sans_chn.groupby(["year"]).agg({"value_trln_USD": "sum", "quantity_mln_metric_tons": "sum"})
+    ratio = total_imports.div(total_imp_sans)
+    total_exports.reset_index(inplace=True)
+    total_imports.reset_index(inplace=True)
+    total_imp_sans.reset_index(inplace=True)
+    ratio.reset_index(inplace=True)
+    to_csv = {"year": total_exports["year"],
+              "imports_trln_USD": total_imports["value_trln_USD"],
+              "imports_mln_metric_tons": total_imports["quantity_mln_metric_tons"],
+              "exports_trln_USD": total_exports["value_trln_USD"],
+              "exports_mln_metric_tons": total_exports["quantity_mln_metric_tons"],
+              "balance_trln_USD": total_exports["value_trln_USD"] - total_imports["value_trln_USD"],
+              "balance_mln_metric_tons": total_exports["quantity_mln_metric_tons"] - total_imports["quantity_mln_metric_tons"],
+              "ratio_of_total_imports_usd": ratio["value_trln_USD"],
+              "ratio_of_total_imports_weight": ratio["quantity_mln_metric_tons"],
+              }
+    save_dataframe_to_csv(pd.DataFrame(to_csv), f"{OUTPUT_DIR_COUNTRY}/CHN/fuel_trade.csv")
+    
+    topn = 10
+    largest_sources = make_human_readable(imports.copy(), cc_df, epc22_df, country_fmt="country_iso3")
+    largest_sources = get_top_partners(largest_sources, "exporter", year=2023, keep_top_n=topn)
+    largest_sources.drop(columns=["product_chapter"], inplace=True)
+    save_dataframe_to_csv(largest_sources, f"{OUTPUT_DIR_COUNTRY}/CHN/top{topn}_sources.csv")
+
+    topn = 15
+    largest_sources = make_human_readable(imports.copy(), cc_df, epc22_df, country_fmt="country_iso3")
+    largest_sources = get_top_partners(largest_sources, "exporter", year=2023, keep_top_n=topn)
+    largest_sources.drop(columns=["product_chapter"], inplace=True)
+    save_dataframe_to_csv(largest_sources, f"{OUTPUT_DIR_COUNTRY}/CHN/top{topn}_sources.csv")
+
+
 def produce_country_specific_csvs(data_df, epc22_df, pc_df, cc_df, gdp_df, cpi_df):
+    china(data_df, epc22_df, pc_df, cc_df, gdp_df, cpi_df)
+    usa(data_df, epc22_df, pc_df, cc_df, gdp_df, cpi_df)
+    iran(data_df, epc22_df, pc_df, cc_df, gdp_df, cpi_df)
     ukraine(data_df, epc22_df, pc_df, cc_df, gdp_df, cpi_df)
     yemen(data_df, epc22_df, pc_df, cc_df, gdp_df, cpi_df)
     saudi_arabia(data_df, epc22_df, pc_df, cc_df, gdp_df, cpi_df)
